98c69d50e313f2755000390364f0a7148cf632a6
pczhangyu <13717713687@163.com>
2019-06-24 09:46:18 +0800

    web

---
+++ marcopolo-web/src/main/java/com/feecn/marcopolo/web/MarcopoloWebApplication.java
@@ -0,0 +1,13 @@
+package com.feecn.marcopolo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcopoloWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcopoloWebApplication.class, args);
+    }
+
+}
